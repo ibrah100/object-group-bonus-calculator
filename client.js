f6fb0a3679e27e25c56b5c4210d53192f1c1dd5a
ibrah100 <37915317+ibrah100@users.noreply.github.com>
2022-11-29 14:48:08 -0600

    finished assignment

--- client.js
+++ client.js
@@ -48,10 +48,69 @@ console.log('array of employee data: ',  employees );
 
 // This function will calculate 1 employee's bonus!
 //
+
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
+
+  employee.bonusPercentage;
+
+  if (employee.reviewRating <= 2){
+    employee.bonusPercentage = 0;
+  } else if (employee.reviewRating === 3){
+    employee.bonusPercentage = 4;
+  } else if (employee.reviewRating === 4){
+    employee.bonusPercentage = 6;
+  } else if (employee.reviewRating === 5){
+    employee.bonusPercentage = 10;
+  }
+
+  if (employee.employeeNumber.length === 4){
+    employee.bonusPercentage += 5;
+  }
+
+  if (employee.annualSalary > 65000){
+    employee.bonusPercentage -= 1;
+  }
+
+  let maxBonus = 13;
+  let minBonus = 0;
+  
+  if (employee.bonusPercentage > maxBonus){
+    employee.bonusPercentage = 13;
+  } else if (employee.bonusPercentage < 0){
+    employee.bonusPercentage = 0;
+  }
+ 
+
+  employee.totalBonus = Math.round(calculateTotalBonus(employee.annualSalary, employee.bonusPercentage));
+  employee.totalComp = Math.round(calculateTotalComp(employee.totalBonus, employee.annualSalary));
+
+
+  return employee;
   
   
   // return new object with bonus results
 
 }
+
+function calculateTotalBonus(annualSalary,bonusPercentage){
+  let totalBonus = annualSalary * (bonusPercentage/100);
+  return totalBonus;
+}
+
+function calculateTotalComp(totalBonus,annualSalary){
+  let totalComp = totalBonus + parseInt(annualSalary);
+  return totalComp;
+}
+
+console.log(calculateIndividualEmployeeBonus(employees[1]));
+console.log(calculateIndividualEmployeeBonus(employees[2]));
+
+let result = calculateTotalBonus(employees[1].annualSalary, employees[1].bonusPercentage);
+
+console.log(result);
+
+for (let emp of employees){
+  console.log(calculateIndividualEmployeeBonus(emp));
+}
+
